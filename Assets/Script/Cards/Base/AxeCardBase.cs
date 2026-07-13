b8c4312a85f62ba0470d2ee8abe8c77ed83e2e5c
Mao_mao <3247749068@qq.com>
2026-07-13 14:38:41 +0800

    将卡牌更改成用json文件存储的静态形式，删除了所有的cs卡牌代码

--- Assets/Script/Cards/Base/AxeCardBase.cs
+++
@@ -1,7 +0,0 @@
-namespace KiKs.Cards
-{
-    public abstract class AxeCardBase : MeleeWeaponCardBase
-    {
-        public sealed override CardFamily Family => CardFamily.Axe;
-    }
-}
--- Assets/Script/Cards/Base/ChainWeaponCardBase.cs
+++
@@ -1,7 +0,0 @@
-namespace KiKs.Cards
-{
-    public abstract class ChainWeaponCardBase : MeleeWeaponCardBase
-    {
-        public sealed override CardFamily Family => CardFamily.ChainWeapon;
-    }
-}
